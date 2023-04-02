e0ff2a95bcf2415c45db7b4c40c78a23f62845bc
Nilesh <Nilesh@192.168.1.56>
2023-04-02 22:10:36 +0400

    My Second Push For testingGitHub

---
+++ MyJavaSeleniumProject/src/JavaSelenium/TestingGitHub.java
@@ -0,0 +1,14 @@
+package JavaSelenium;
+
+public class TestingGitHub {
+
+	public TestingGitHub() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Testing for GitHub");
+	}
+
+}
